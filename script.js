4b5a1f3da19fcdd076d6348ef371bfca6220bd4d
s241003 <s241003@kashiwanoha.ed.jp>
2025-06-23 13:35:29 +0900

    音声
    読み上げ

---
+++ script.js
@@ -0,0 +1,69 @@
+let questions = [];
+let currentIndex = 0;
+
+async function fetchQuestions() {
+  const res = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&#39');
+  const data = await res.json();
+  questions = data.results;
+  showQuestion();
+}
+
+function decodeHTMLEntities(text) {
+  const textarea = document.createElement("textarea");
+  textarea.innerHTML = text;
+  return textarea.value;
+}
+
+function showQuestion() {
+  if (currentIndex >= questions.length) {
+    document.getElementById('quiz').innerHTML = '<h2>クイズ終了！</h2>';
+    return;
+  }
+
+  const q = questions[currentIndex];
+  const questionText = decodeHTMLEntities(q.question);
+  const options = [...q.incorrect_answers, q.correct_answer];
+  shuffleArray(options);
+
+  document.getElementById('question').textContent = questionText;
+  readText(questionText);
+  const optionsContainer = document.getElementById('options');
+  optionsContainer.innerHTML = '';
+
+  options.forEach(option => {
+    const btn = document.createElement('button');
+    btn.className = 'option-btn';
+    btn.textContent = decodeHTMLEntities(option);
+    btn.onclick = () => {
+      if (option === q.correct_answer) {
+        readText("correct!");
+        alert('正解！');
+        currentIndex++;
+        showQuestion();
+      } else {
+        readText("Failed! The answer is " + q.correct_answer);
+        alert('不正解!もう一度選んでください');
+      }
+
+    };
+    optionsContainer.appendChild(btn);
+  });
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function readText(text) {
+  // 読み上げ機能
+  const utterance = new SpeechSynthesisUtterance(text);
+  const rate = document.getElementById("rate").value;
+  utterance.lang = "en-US"; // アメリカ英語の音声で読み上げ
+  utterance.rate = rate;
+  speechSynthesis.speak(utterance);
+}
+
+fetchQuestions();
