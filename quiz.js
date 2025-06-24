24dd7dd680c6e86db7c914db813932514ffe07a3
s241002 <s241002@kashiwanoha.ed.jp>
2025-06-24 10:58:22 +0900

    commit_4
    bug fix_2

--- quiz.js
+++ quiz.js
@@ -14,17 +14,38 @@ async function fetchQuestions() {
 
 function decodeHTMLEntities(text) {
   const textarea = document.createElement("textarea");
+// quiz.js07
+let questions = [];
+let currentIndex = 0;[]
+let num = [0,1,2,3,4,5,6,7,8,9];
+let badanswer = 0;
+let score = 0;
+let badcheck = 0;
+
+
+async function fetchQuestions() {
+  const res = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&#39;');
+  const data = await res.json();
+  questions = data.results;
+  showQuestion();
+}
+
+function decodeHTMLEntities(text) {
+  const textarea = document.createElement("textarea");
   textarea.innerHTML = text;
   return textarea.value;
 }
 
 function showQuestion() {
-  if (currentIndex >= num.length) {
- 	  window.alert('クイズ終了！あなたの不正解を押した数は'　+ badanswer + '回でした！あなたの正解数は' + score + '回でした！');
-      document.getElementById('quiz').innerHTML = '<h2>クイズ終了！</h2>';
-      return;
-  };
-  const q = questions[num[currentIndex]];
+  if (currentIndex >= questions.length) {
+    window.alert('クイズ終了！あなたの不正解を押した数は'　+ badanswer + '回でした！あなたの正解数は' + score + '回でした！');
+    document.getElementById('quiz').innerHTML = 
+      '<h2>クイズ終了！</h2>';
+    return;
+  }
+
+
+  const q = questions[currentIndex];
   const questionText = decodeHTMLEntities(q.question);
   const options = [...q.incorrect_answers, q.correct_answer];
   shuffleArray(options);
@@ -41,10 +62,11 @@ function showQuestion() {
       if (option === q.correct_answer) {
         alert('正解！');
         currentIndex++;
-        if (badcheck == 0) {
+        if (badcheck == 0)
+        {
           badcheck = 0;
           score = score + 1;
-        } else {
+        }else{
           badcheck = 0;
         }
         showQuestion();
@@ -54,13 +76,14 @@ function showQuestion() {
         if (badcheck == 0){
           badcheck = 1;
         }
-      };
-      optionsContainer.appendChild(btn);
+      }
     };
+    optionsContainer.appendChild(btn);
     if (currentIndex > 9) {
       procrastinate.style.display="block";
     }
   });
+}
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
