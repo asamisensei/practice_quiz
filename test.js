c219ec571a278543591949cb9d830765b42ac1e3
s241002 <s241002@kashiwanoha.ed.jp>
2025-06-30 12:15:18 +0900

    shusei

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-// quiz.js07
 let questions = [];
 let currentIndex = 0;
 let num = [0,1,2,3,4,5,6,7,8,9];
@@ -8,7 +7,7 @@ let badcheck = 0;
 
 
 async function fetchQuestions() {
-  const res = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&#39;');
+  const res = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple');
   const data = await res.json();
   questions = data.results;
   showQuestion();
@@ -23,53 +22,9 @@ function decodeHTMLEntities(text) {
 function showQuestion() {
   if (currentIndex >= num.length) {
     window.alert('クイズ終了！あなたの不正解を押した数は'　+ badanswer + '回でした！あなたの正解数は' + score + '回でした！');
-    document.getElementById('quiz').innerHTML = 
-      '<h2>クイズ終了！</h2>';
+    document.getElementById('quiz').innerHTML = '<h2>クイズ終了！</h2>';
     return;
   }
-
-
-  const q = questions[num[currentIndex]];
-  const questionText = decodeHTMLEntities(q.question);
-  const options = [...q.incorrect_answers, q.correct_answer];
-  shuffleArray(options);
-
-  document.getElementById('question').textContent = questionText;
-  const optionsContainer = document.getElementById('options');
-  optionsContainer.innerHTML = '';
-
-  options.forEach(option => {
-    const btn = document.createElement('button');
-// quiz.js07
-let questions = [];
-let currentIndex = 0;
-let num = [0,1,2,3,4,5,6,7,8,9];
-let badanswer = 0;
-let score = 0;
-let badcheck = 0;
-
-
-async function fetchQuestions() {
-  const res = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&#39;');
-  const data = await res.json();
-  questions = data.results;
-  showQuestion();
-}
-
-function decodeHTMLEntities(text) {
-  const textarea = document.createElement("textarea");
-  textarea.innerHTML = text;
-  return textarea.value;
-}
-
-function showQuestion() {
-  if (currentIndex >= num.length) {
-    window.alert('クイズ終了！あなたの不正解を押した数は'　+ badanswer + '回でした！あなたの正解数は' + score + '回でした！');
-    document.getElementById('quiz').innerHTML = 
-      '<h2>クイズ終了！</h2>';
-    return;
-  }
-
 
   const q = questions[num[currentIndex]];
   const questionText = decodeHTMLEntities(q.question);
